6e1e4f978ac32b94a03cae43856128359f3451ac
christian <delormechrist@gmail.com>
2021-05-03 23:22:36 +0200

    ad grid

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,8 +1,6 @@
 <template>
-  <!-- <h1>My Projects</h1> -->
   <div class="wrapper">
-    <!-- <h1>Projects</h1>
-    <div class="tarjetas">
+    <!-- <div class="tarjetas">
 
 
       <div class="tarjetas__item">
@@ -18,56 +16,24 @@
           <p><small>Incluido IGV.</small></p>
         </div> -->
 
-      <!-- <article class="project-listing">
-        <a class="project-link" href="https://github.com/krissdel/Rextense" target="_blank">Lien Github</a>
-        <div class="project-image">
-          <a href="https://rextense.com/" target="_blank"><img src="../assets/rextense.png" alt="image rextense "/> </a>
-        </div>
-      </article> -->
-
-<!-- 
-      </div>
+     
+      <!-- </div>
 
 
-
-
-      <div class="tarjetas__item">
-        <h2 class="tarjetas__titulo">Página Simple</h2>
-
-        <div class="tarjetas__info">
-          <p>Una web con diferentes secciones.</p>
-        </div>
-        <div class="tarjetas__precio">
-          <p>S/. 2950.00</p>
-          <p><small>Incluido IGV.</small></p>
-        </div>
-      </div>
-      <div class="tarjetas__item">
-        <h2 class="tarjetas__titulo">Tienda virtual</h2>
-
-        <div class="tarjetas__info">
-          <p>
-            Has crecer tu negocio con una tienda digital que acepta todo tipo de
-            tarjetas
-          </p>
-        </div>
-        <div class="tarjetas__precio">
-          <p>S/. 2950.00</p>
-          <p><small>Incluido IGV.</small></p>
-        </div>
-      </div>
-    </div>
-  </div> -->
+      
+    </div> -->
+  <!-- </div>  -->
   <!-- ================================= -->
   <div class="grid-container">
       <!-- <Loader /> -->
 
     <div class="child-page-listing">
-      <h2>projects</h2>
-
+     
+<h1>projects</h1>
 
 
       <article class="project-listing">
+        <h2 class="project-title">Rextense</h2>
         <a class="project-link" href="https://github.com/krissdel/Rextense" target="_blank">Lien Github</a>
         <div class="project-image">
           <a href="https://rextense.com/" target="_blank"><img src="../assets/rextense.png" alt="image rextense "/> </a>
@@ -79,6 +45,8 @@
 
 
       <article class="project-listing">
+                <h2 class="project-title">Groupomania</h2>
+
         <a class="project-link" href="https://github.com/krissdel/groupomania">Lien Github</a>
         <div class="project-image">
           <a href="#">
@@ -88,6 +56,7 @@
       </article>
 
       <article class="project-listing">
+                        <h2 class="project-title">So Pekocko</h2>
         <a class="project-link" href="#">Lien Github</a>
         <div class="project-image">
           <a href="#">
@@ -97,6 +66,7 @@
       </article>
 
       <article class="project-listing">
+       <h2 class="project-title">Orinoco</h2>
         <a class="project-link" href="#">Lien Github</a>
         <div class="project-image">
           <a href="#">
@@ -105,6 +75,7 @@
         </div>
       </article>
       <article class="project-listing">
+               <h2 class="project-title">Chouette Agence</h2>
         <a class="project-link" href="#">Lien Github</a>
         <div class="project-image">
           <a href="#">
@@ -114,6 +85,7 @@
       </article>
 
       <article class="project-listing">
+               <h2 class="project-title">Ohmyfood</h2>
         <a class="project-link" href="#">Lien Github</a>
         <div class="project-image">
           <a href="#"> <img src="../assets/ohmyfood2.png" alt="image ohmyfood"/> </a>
@@ -121,6 +93,8 @@
       </article>
 
       <article class="project-listing">
+                       <h2 class="project-title">CV</h2>
+
         <a class="project-link" href="#">Lien Github</a>
         <div class="project-image">
           <a href="#"> <img src="../assets/cv2.png" alt="image projet CV"/> </a>
@@ -147,20 +121,19 @@ export default {
 <style scoped>
 .wrapper {
   max-width: 900px;
-  font-family: Lato, sans-serif;
   margin: auto auto;
   padding: 1em;
 
 }
 
-img {
-  width: 100%;
-  height: auto;
-}
+
 
 .grid-container {
   display: grid;
+  /* display: inline-grid; */
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    /* grid-template-rows: auto 1.5fr 1fr; */
+
   grid-gap: 1em;
   margin-left: 10pc;
   animation: fade-in-up 2s backwards;
@@ -168,69 +141,36 @@ img {
 h1 {
   color: rgb(17, 236, 229);
   margin-top: 10%;
-  font-family: Josefin Sans, sans-serif;
+  
 }
-
-/* ==================================== */
-
-/* @import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"); */
-
-
-
-
-
-.tarjetas {
+.project-listing {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-template-rows: auto 1.5fr 1fr;
-  gap: 2rem;
-}
-.tarjetas__item {
-  display: grid;
-  box-shadow: 0px 0px 40px 0px rgba(0, 0, 0, 0.4);
+  box-shadow: rgb(17 236 229) 0px 0px 6px 0.7px;
   border-radius: 70px 10px 10px 10px;
   text-align: center;
   padding: 1em;
   grid-template-rows: subgrid;
   grid-row: span 3;
   grid-row-gap: 0em;
-  background: linear-gradient(90deg, white 0%, #f1f1f1 100%);
+  /* background: linear-gradient(90deg, white 0%, #0f1724 100%); */
+  background: linear-gradient(90deg, white 0%, #11ece5 100%);
+
   transition: all 500ms ease-in-out;
   animation: fade-in-up 2s backwards;
-
-  /* @for $i from 1 through 3 {
-    &:nth-child(#{$i}n) {
-      animation-delay: #{$i * 0.2}s;
-    }
-  }
-
-  &:hover {
-    transform: translate(0, -1.5rem) rotate(-3deg) scale(1.3);
-  } */
+  margin: 1em;
 }
-.tarjetas__titulo {
+.project-listing:hover{
+  transform: translate(0, -1.5rem) rotate(-3deg) scale(1.3);
+}
+.project-title {
   font-size: 2rem;
-  grid-row: span 1;
-  font-family: Josefin Sans, sans-serif;
+  /* grid-row: span 1; */
 }
-.tarjetas__info {
-  grid-row: span 1;
-  max-width: 22ch;
-  margin: 0 auto;
+img {
+  width: 100%;
+  height: auto;
 }
-.tarjetas__precio {
-  grid-row: span 1;
-  font-size: 2rem;
-  font-weight: 900;
-}
-.tarjetas__precio p {
-  margin: 0;
-}
-.tarjetas__precio small {
-  font-size: 0.8rem;
-  font-weight: 300;
-}
+/* ==================================== */
 
 /* // Animations */
 
